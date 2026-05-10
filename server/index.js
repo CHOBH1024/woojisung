2e076c10ca39cd53287440f8fd4d3dd6bc7bb92d
MirrorInsightDeployer <deployer@mirror-insight.pro>
2026-05-10 21:20:36 +0900

    Updates

--- server/index.js
+++
@@ -1,9 +0,0 @@
-import { z, A } from "./assets/worker-entry-CekZxYvN.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream";
-import "node:stream/web";
-export {
-  z as createServerEntry,
-  A as default
-};
